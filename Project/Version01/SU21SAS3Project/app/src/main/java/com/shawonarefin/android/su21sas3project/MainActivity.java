fe2c137b226110065dd0e0d09540508a4da3dd4a
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2021-06-20 18:06:10 +0600

    Initial commit

---
+++ Project/Version01/SU21SAS3Project/app/src/main/java/com/shawonarefin/android/su21sas3project/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shawonarefin.android.su21sas3project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    ///Test comment
+    setContentView(R.layout.activity_main);
+  }
+}
